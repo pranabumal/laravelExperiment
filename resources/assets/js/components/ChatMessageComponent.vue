d4358493664247016507a68e1de00761a17c250a
pranab <sarker.pranab@genweb2.com>
2018-12-03 17:45:42 +0600

    Chat section Updated!

---
+++ resources/assets/js/components/ChatMessageComponent.vue
@@ -0,0 +1,24 @@
+// ChatMessagesComponent.vue
+
+<template>
+    <div class="message-area">
+        <message-component></message-component>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+<style>
+    .message-area {
+        height: 400px;
+        max-height: 400px;
+        overflow-y: scroll;
+        padding: 15px;
+        border-bottom: 1px solid #eee;
+    }
+</style>
